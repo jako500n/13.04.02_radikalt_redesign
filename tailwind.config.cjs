542c1249c0274072a90b16755ef9ff03094c6187
”Jakob <jako500n@stud.kea.dk>
2022-10-07 09:34:33 +0200

    hey

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["./src/**/*.{html,vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        main: "#14272B",
+        lilla: "#5252DF",
+        beige: "#FFB486",
+        rod: "#FF6445",
+        groen: "#2F6457",
+      },
+    },
   },
   plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms")],
 };
