da9e60a848038c29b676bee8e5efb7a2d51c3e87
”Christopher <chri26kx@stud.kea.dkgit config --global user.email chri26kx@stud.kea.dk>
2022-10-11 14:26:40 +0200

    toggle

--- src/main.js
+++ src/main.js
@@ -1,1 +1,22 @@
 import "./style.css";
+
+// skal nok laves om til element child ?!
+const vis = document.querySelector(".vis");
+const vis2 = document.querySelector(".vis2");
+const vis3 = document.querySelector(".vis3");
+
+const klikker = document.querySelector(".klikker");
+const klikker2 = document.querySelector(".klikker2");
+const klikker3 = document.querySelector(".klikker3");
+
+klikker.addEventListener("click", (e) => {
+  vis.classList.toggle("hidden");
+});
+
+klikker2.addEventListener("click", (e) => {
+  vis2.classList.toggle("hidden");
+});
+
+klikker3.addEventListener("click", (e) => {
+  vis3.classList.toggle("hidden");
+});
