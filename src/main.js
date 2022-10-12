de31c7b696bf7a919354998309c6b28e6c3fca17
”Christopher <chri26kx@stud.kea.dkgit config --global user.email chri26kx@stud.kea.dk>
2022-10-12 17:51:09 +0200

    arrow

--- src/main.js
+++ src/main.js
@@ -11,14 +11,26 @@ const klikker3 = document.querySelector(".klikker3");
 
 klikker.addEventListener("click", (e) => {
   vis.classList.toggle("hidden");
+  var arrow = document.querySelector(".arrow");
+  var arrow_up = document.querySelector(".arrow_up");
+
+  arrow.classList.toggle("arrow_up");
 });
 
 klikker2.addEventListener("click", (e) => {
   vis2.classList.toggle("hidden");
+  var arrow2 = document.querySelector(".arrow2");
+  var arrow_up2 = document.querySelector(".arrow_up2");
+
+  arrow2.classList.toggle("arrow_up2");
 });
 
 klikker3.addEventListener("click", (e) => {
   vis3.classList.toggle("hidden");
+  var arrow3 = document.querySelector(".arrow3");
+  var arrow_up3 = document.querySelector(".arrow_up3");
+
+  arrow3.classList.toggle("arrow_up3");
 });
 
 // const openMenu = document.querySelector(".openMenu");
